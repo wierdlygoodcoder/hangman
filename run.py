15b31a50f039e2e53a0e7263e3fc39f03751b534
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-05-04 23:07:12 +0000

    added a way for the program to list the guessed letters

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
 
-#The secret word the player is trying to guess
+# The secret word the player is trying to guess
 secertword = "code"
 lettersGuessed = ""
 
-#The number of turns before the player loses
+# The number of turns before the player loses
 losecount = 5
 
-#loop until the player has made too many faild tries
-#will breack loop if they succeed instead
+# loop until the player has made too many faild tries
+# will breack loop if they succeed instead
 while losecount > 0:
 
-    #get the players guessed letters
+    # get the players guessed letters
     guess = input("Enter a letter: ")
 
     if guess in secertword:
-        #player guessed correctly
+        # player guessed correctly
         print(f"Correct! there is {guess} more letters in the secret word.")
     else:
         losecount -= 1
-        print(f"incorrect. there are no{guess} in the secert word. you have {losecount} tries left.")
+        print(f"incorrect. there are no{guess} in the secert word. You have {losecount} tries left.")
+
+    # makes a list of all letters guessed
+    lettersGuessed = lettersGuessed + guess
+    wrongLetterCount = 0
