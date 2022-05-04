066a90e03521859ee51a2a0403674b3c20384411
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-05-04 23:00:16 +0000

    added the guess loop

--- run.py
+++ run.py
@@ -8,6 +8,14 @@ losecount = 5
 
 #loop until the player has made too many faild tries
 #will breack loop if they succeed instead
-while losecount > 0;
+while losecount > 0:
 
-    
+    #get the players guessed letters
+    guess = input("Enter a letter: ")
+
+    if guess in secertword:
+        #player guessed correctly
+        print(f"Correct! there is {guess} more letters in the secret word.")
+    else:
+        losecount -= 1
+        print(f"incorrect. there are no{guess} in the secert word. you have {losecount} tries left.")
