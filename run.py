c13701c4a1b6ae0b8c665b204561afe465d5a6b7
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-05-05 23:30:51 +0000

    added libarys and basic choose difficulties

--- run.py
+++ run.py
@@ -1,52 +1,89 @@
+import random
+
+easy = {
+     "word_1": "apple",
+     "word_2": "pear",
+     "word_3": "grape",
+     }
+# test_easy = random.choice(list(easy.values()))
+normal = {
+    "word_1": "green",
+    "word_2": "blue",
+    "word_3": "orange"
+    }
+# test_normal = random.choice(list(normal.values()))
+hard = {
+    "word_1": "water",
+    "word_2": "juice",
+    "word_3": "coffee"
+    }
+# test_hard = random.choice(list(hard.values()))
+# print(test_easy, test_normal, test_hard)
+difficulty = {
+    "easy": easy,
+    "normal": normal,
+    "hard": hard
+}
+
 # The secret word the player is trying to guess
-secertWord = "arcade"
-lettersGuessed = ""
+secert_word = difficulty.values()
+letters_guessed = ""
+
 
 # The number of turns before the player loses
-losecount = 5
+lose_count = 5
 
 # loop until the player has made too many faild tries
 # will breack loop if they succeed instead
-while losecount > 0:
+while lose_count > 0:
+
+    choose_difficulty = input("choose you difficulty Easy Normal or Hard!!")
+    if choose_difficulty == "easy":
+        num = random.choice(list(easy.values()))
+        print(choose_difficulty, num)
 
     # get the players guessed letters
     guess = input("Enter A letter: ")
-    allowedCharacters = 'abcdefghijklmnopqrstuvwxyz'
-    while(len(guess) != 1 or guess not in allowedCharacters):
+    allowed_characters = 'abcdefghijklmnopqrstuvwxyz'
+    while(len(guess) != 1 or guess not in allowed_characters):
         guess = input("Thats not a letter, Enter A Letter: ")
 
-    if guess in secertWord:
+    if guess in secert_word:
         # player guessed correctly
         print(f"Correct! there is {guess} more letters in the secret word.")
     else:
-        losecount -= 1
-        print(lettersGuessed)
+        lose_count -= 1
+        print(letters_guessed)
         print(
             f"incorrect. there are no {guess} in the secert word, "
-            f"You have {losecount} tries left."
+            f"You have {lose_count} tries left."
             )
-    if guess in lettersGuessed:
+    if guess in letters_guessed:
         print("You have already guessed this letter, please try again.")
 
     # makes a list of all letters guessed
-    lettersGuessed = lettersGuessed + guess
-    wrongLetterCount = 0
+    letters_guessed = letters_guessed + guess
+    wrong_letter_count = 0
 
-    for letter in secertWord:
-        if letter in lettersGuessed:
-            print(f" {letter} ", end="")
+    for index, letter in enumerate(secert_word):
+        if letter in letters_guessed:
+            if index == 0:
+
+                print(f" {letter.capitalize()}", end="")
+            else:
+                print(f" {letter}", end="")
         else:
             print(" _ ", end="")
-            wrongLetterCount += 1
+            wrong_letter_count += 1
     print("")
     # if there are not lossed letters then the player won
-    if wrongLetterCount == 0:
-        print(f" Well Done! the secret word was: {secertWord}. You Win")
+    if wrong_letter_count == 0:
+        print(f" Well Done! the secret word was: {secert_word}. You Win")
         break
 
 
 else:
     print(
         "Sorry, you failed and did not win."
-        f"The word was {secertWord} Try Again"
+        f"The word was {secert_word} Try Again"
         )
