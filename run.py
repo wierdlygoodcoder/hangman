176cf9b64ca591adb886981f090450b13eb464da
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-05-12 23:08:28 +0000

    fixed tthe functions so that the last two functions repeat

--- run.py
+++ run.py
@@ -13,73 +13,79 @@ LETTERS_GUESSED = ""
 LOSE_COUNT = 6
 
 
-def choose_difficulty(difficulty):
+def choose(difficulty):
     """
-    loop until the player has made too many faild tries
-    will breack loop if they succeed instead
+    loop until the player has made too many failed tries
+    will break loop if they succeed instead
     """
     choose_difficulty = input(
         "choose you difficulty Easy, Normal or Hard!!"
         ).lower()
+
+    easy_word = random.choice(difficulty["easy"])
+    normal_word = random.choice(difficulty["normal"])
+    hard_word = random.choice(difficulty["hard"])
     while not (
         choose_difficulty == "easy"
         or choose_difficulty == "normal"
         or choose_difficulty == "hard"
     ):
-        easy_word = random.choice(difficulty["easy"])
-        normal_word = random.choice(difficulty["normal"])
-        hard_word = random.choice(difficulty["hard"])
+
         choose_difficulty = input(
             "choose you difficulty Easy,Normal or Hard!!"
             ).lower()
-        print(choose_difficulty, easy_word, normal_word, hard_word)
+    return [easy_word, normal_word, hard_word, choose_difficulty]
 
 
-def loses(lose_count, normal_word, easy_word, hard_word, secret_word):
+def loses(normal_word, easy_word, hard_word, choose_difficulty):
     """
-    player gets to choose diiculty
+    a player gets to choose difficulty
     """
-    while lose_count > 0:
-        if choose_difficulty == "normal":
-            secret_word = normal_word
-        elif choose_difficulty == "easy":
-            secret_word = easy_word
-        else:
-            secret_word == hard_word
+    if choose_difficulty == "normal":
+        secret_word = normal_word
+    elif choose_difficulty == "easy":
+        secret_word = easy_word
+    else:
+        secret_word = hard_word
     return secret_word
 
 
-def guesses(secret_word, lose_count, LETTERS_GUESSED):
-    """ """
+def guesses(secret_word, LETTERS_GUESSED):
+    """
+    allows users to have a guess
+    """
+    global LOSE_COUNT
     guess = input("Enter A letter: ")
     allowed_characters = "abcdefghijklmnopqrstuvwxyz"
+
     while len(guess) != 1 or guess not in allowed_characters:
         guess = input("Thats not a letter, Enter A Letter: ")
-
     if guess in secret_word:
-        # player guessed correctly
-        print(f"Correct! there is {guess} more letters in the secret word.")
-    else:
-        lose_count -= 1
-        print(guess)
         print(
-            f"incorrect. there are no {guess} in the secert word, "
-            f"You have {lose_count} tries left."
+            f"Correct! {guess} in the secret word."
+            )
+    else:
+        LOSE_COUNT -= 1
+        print(
+            f"incorrect. there are no {guess} in the secert word,"
+            f"You have {LOSE_COUNT} tries left."
         )
     if guess in LETTERS_GUESSED:
         print("You have already guessed this letter, please try again.")
 
     # makes a list of all letters guessed
+    return win_guess(LETTERS_GUESSED, guess, secret_word)
 
 
-def letters_guess(LETTERS_GUESSED, guess, secret_word):
+def win_guess(LETTERS_GUESSED, guess, secret_word):
+    """
+    checking to see if the player has won that round
+    """
     LETTERS_GUESSED = LETTERS_GUESSED + guess
     wrong_letter_count = 0
-
     for index, letter in enumerate(secret_word):
         if letter in LETTERS_GUESSED:
             if index == 0:
-
                 print(f" {letter.capitalize()}", end="")
             else:
                 print(f" {letter}", end="")
@@ -89,22 +95,26 @@ def letters_guess(LETTERS_GUESSED, guess, secret_word):
     print("")
     # if there are not lossed letters then the player won
     if wrong_letter_count == 0:
-        print(f" Well Done! the secret word was: {secret_word}. You Win")
-
+        print(f"Well Done! the secret word was: {secret_word}. You Win")
+    elif LOSE_COUNT == 0:
+        print("YOU LOOSE!!")
+        print(f"The secret word was {secret_word}")
     else:
-        print(f"Sorry, you failed. The word was {secret_word} Try Again")
+        print("There are still letters left. Please guess another letter")
+        return guesses(secret_word, LETTERS_GUESSED)
 
 
 def main():
     """
     run all programs functions
     """
-    choose_difficulty(difficulty)
-    loses(
-        lose_count, normal_word, easy_word, hard_word, secret_word
+    easy_word, normal_word, hard_word, choose_difficulty = choose(difficulty)
+    secret_word = loses(
+         normal_word, easy_word, hard_word, choose_difficulty
         )
-    guesses(secret_word, lose_count, LETTERS_GUESSED)
-    letters_guess(LETTERS_GUESSED, guess, secret_word)
+    guesses(secret_word, LETTERS_GUESSED)
+    return main()
 
 
-main()
+if __name__ == "__main__":
+    main()
